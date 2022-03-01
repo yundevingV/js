5c984474580ad80e26dfb6c0dc2bade905cfcdbe
ys0222 <91381230+ys0222@users.noreply.github.com>
2022-03-01 16:41:05 +0900

    Update 0301.js

--- 2022-03/0301.js
+++ 2022-03/0301.js
@@ -6,8 +6,10 @@
 1.라디오 클릭시 클릭한 값 바로 출력하기.
 
 index.html
-<input type="radio" value="female" name="gender" onclick="GenderCheck(event)">여성</input>
-<input type="radio" value="male" name="gender" onclick="GenderCheck(event)">남성</input>
+<input type="radio" value="female" name="gender" id="g">
+<label for="g">여성</label>
+<input type="radio" value="male" name="gender" id="g1">
+<label for="g1">남성</label>
  
 <div id="result"></div>
 
@@ -26,6 +28,15 @@ document.getElementById('result')를 통해 html코드에서 id를 result 요소
 
 2. 라디오 버튼 값을 선택한 후, 출력 버튼 클릭시 값 출력하기
 
+index.html
+ <input type="radio" value="female" name="gender" id="g">
+<label for="g">여성</label>
+<input type="radio" value="male" name="gender" id="g1">
+<label for="g1">남성</label>
+
+<button onclick="GenderCheck()">확인하기</button>
+
+Radio.js
 function GenderCheck() {
 
     const gender = document.getElementsByName('gender');
@@ -39,5 +50,7 @@ function GenderCheck() {
 const gender = document.getElementsByName('gender');
 을 통해 라디오버튼 목록을 다 gender라는 변수에 저장합니다.
 forEach구문을 통해서 gender목록을 순회하면서 체크되어있는값이면 value값을 출력하게끔합니다.
+
+
 
 출처: https://hianna.tistory.com/426
